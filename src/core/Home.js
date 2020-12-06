17699f459f630f324e1d1e653c5bf3113d4b3c2c
SuNg Do <sungdo1004@gmail.com>
2020-12-06 00:05:31 -0800

    signup handle change

--- src/core/Home.js
+++ src/core/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Layout from './Layout'
 
 const Home = () => (
-    <Layout title="Welcome to 4scent Store" description="We carry high-quality scented products for cars and home decor">
+    <Layout title="Welcome to 4scent Store" description="We carry high-quality scented products for car and home decor">
         ....
     </Layout>
 )
