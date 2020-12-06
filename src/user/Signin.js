2ef245b31bfb443a1a51a673cce7312074de7bbf
SuNg Do <sungdo1004@gmail.com>
2020-12-06 01:11:41 -0800

    signin workign

--- src/user/Signin.js
+++ src/user/Signin.js
@@ -1,8 +1,90 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { Redirect } from 'react-router-dom'
 import Layout from '../core/Layout'
+import {signin} from '../auth'
 
-const Signin = () => (
-    <Layout title="Signin" description="Sign in to 4scent store"></Layout>
-)
+const Signin = () => {
+    const [values, setValues] = useState({ 
+        email: '',
+        password: '',
+        error: '',
+        loading: false,
+        redirectToReferrer: false,
+
+    })
+
+    const {email, password, loading, error, redirectToReferrer} = values
+
+    const handleChange = name => event => {
+        setValues({...values, error: false, [name]: event.target.value})
+    }
+
+    const clickSubmit = (event) => {
+        event.preventDefault()
+        // so that browser doesn't reload when clicked
+        setValues({...values, error: false, loading: true})
+        signin({email, password})
+        .then(data => {
+            if(data.error) {
+                setValues({...values, error: data.error, loading: false})
+            } else {
+                setValues({
+                    ...values,
+                    redirectToReferrer: true
+                })
+            }
+        })
+
+
+    }
+
+
+
+    const signUpForm = () => (
+        <form>
+            <div className="form-group">
+                <label className='text-muted'>Email</label>
+                <input onChange={handleChange('email')} type="email" className='form-control' value={email}/>
+            </div>
+
+            <div className="form-group">
+                <label className='text-muted'>Password</label>
+                <input onChange={handleChange('password')} type="password" className='form-control' value={password}/>
+            </div>
+            <button onClick={clickSubmit} className='btn btn-primary'>
+               Submit 
+            </button>
+        </form>
+    )
+
+    const showError = () => (
+        <div className='alert alert-danger' style={{display: error ? '' : 'none'}}>
+            {error}
+        </div>)
+
+    const showLOading = () => (
+        loading && (<div className='alert alert-info'><h2>Loading....</h2></div>)
+    )
+
+    const redirectUser = () => {
+        if(redirectToReferrer) {
+            return <Redirect to='/' /> 
+        }
+    }
+
+
+    return (
+        <Layout 
+            title="Signup"
+            description="Signup to 4scent store" 
+            className="container col-md-8 offset-md-2"     
+        >
+        {showLOading()}
+        {showError()}
+        {signUpForm()}
+        {redirectUser()}
+        </Layout> 
+    )
+}
 
 export default Signin
--- src/user/Signup.js
+++ src/user/Signup.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import {Link} from 'react-router-dom'
 import Layout from '../core/Layout'
-import {API} from '../config'
+import {signup} from '../auth'
 
 const Signup = () => {
     const [values, setValues] = useState({
@@ -18,22 +18,7 @@ const Signup = () => {
         setValues({...values, error: false, [name]: event.target.value})
     }
 
-    const signup = (user) => {
-        return fetch(`${API}/signup`, {
-            method: "POST", 
-            headers: {
-                Accept: 'application/json',
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(user)
-        })
-        .then(response => {
-            return response.json()
-        })
-        .catch(err => {
-            console.log(err)
-        })
-    }
+
 
 
 
@@ -91,7 +76,7 @@ const Signup = () => {
 
     const showSuccess = () => (
         <div className='alert alert-info' style={{display: success ? '' : 'none'}}>
-            Your account has been created. Please <Link to='/signin'>sign in</Link>
+            Your account has been created. Please <Link to='/signin'>Signin</Link>
         </div>
     )
 
