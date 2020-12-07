3e773a4e4763dd2af96afd7a990240e7bfc770c1
SuNg Do <sungdo1004@gmail.com>
2020-12-06 23:27:38 -0800

    filter categories init

--- src/core/Home.js
+++ src/core/Home.js
@@ -35,7 +35,7 @@ const Home = () => {
 
 
     return (
-        <Layout title="Welcome to 4scent Store" description="We carry high-quality scented products for car and home decor" className='container-fluid'>
+        <Layout title="Welcome to 4scent store" description="We carry high-quality scented products for car and home decor" className='container-fluid'>
             <h2 className='mb-4'>New Arrivals</h2>
             <div className='row'>
                 {productsByArrival.map((product, i) => (<Card key={i} product={product}/>))}
---
+++ src/core/Shop.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+import Layout from './Layout'
+import Card from './Card'
+import {getCategories} from './apiCore'
+import Checkbox from './Checkbox';
+
+const Shop = () => {
+    const [categories, setCategories] = useState([])
+    const [error, setError] = useState(false)
+
+    const init = () => {
+        getCategories().then(data => {
+            if(data.error) {
+                setError(data.error)
+            } else {
+                setCategories(data)
+            }
+        })
+    }
+
+    useEffect(() => {
+        init()
+    }, [])
+
+    return (
+        <Layout
+            title="Welcome to 4scent store"
+            description = "We carry high-quality scented products for car and home decor"
+            className='container-fluid'
+        >
+            <div className='row'>
+                <div className='col-4'>
+                    <h4>Filter by categories</h4>
+                        <ul>
+                            <Checkbox categories={categories}/>
+                        </ul>
+                </div>
+                <div className='col-8'>
+                    right sidebar
+                </div>
+
+            </div>
+        </Layout>
+    )
+}
+
+export default Shop
